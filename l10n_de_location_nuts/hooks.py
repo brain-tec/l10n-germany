3fb476b3f5f8765ab8127715710b73c94e44e2c4
Andrii Kompaniiets <andrii@moduon.team>
2025-08-25 09:21:19 +0200

    [MIG] l10n_de_location_nuts: Migration to 18.0

--- l10n_de_location_nuts/hooks.py
+++ l10n_de_location_nuts/hooks.py
@@ -10,9 +10,9 @@ from odoo import SUPERUSER_ID, api
 _logger = logging.getLogger(__name__)
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """Define German specific configuration in res.country."""
-    env = api.Environment(cr, SUPERUSER_ID, {})
+    env = api.Environment(env.cr, SUPERUSER_ID, {})
     germany = env.ref("base.de")
     _logger.info("Setting Germany NUTS configuration")
     germany.write({"state_level": 2})
