555aa0599b545ca9a20b343b088fb6ee39a5f837
Holger Brunn <mail@hunki-enterprises.com>
2026-01-30 17:29:32 +0100

    [IMP] datev_export_dtvf: change human readable module name

--- datev_export_dtvf/__manifest__.py
+++ datev_export_dtvf/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Hunki Enterprises BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "DATEV",
+    "name": "Datev Export DTVF",
     "summary": "Export Data for DATEV (dtvf)",
     "version": "18.0.1.0.0",
     "development_status": "Beta",
