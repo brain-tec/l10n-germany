a73834208d6b8098989cf8264d40b9fc2aab9da5
Luis Rodriguez <luis.rodriguez@dixmit.com>
2026-03-19 07:52:23 +0100

    [MIG] l10n_din5008_move_name: migration to 18.0

--- l10n_din5008_move_name/__manifest__.py
+++ l10n_din5008_move_name/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "L10n DIN5008 Move Name",
     "summary": """Add Account move name on the name of the move""",
-    "version": "19.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit, MT Software, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-germany",
